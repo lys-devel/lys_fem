57d688bd5627e80369d2cea89dfa0e5b6778b339
Asuka <lys.dev@gmail.com>
2023-11-20 17:37:21 +0900

    Add stationary test for LLG

--- test/test_llg.py
+++ test/test_llg.py
@@ -8,7 +8,32 @@ from .base import FEMTestCase
 
 T = 2*np.pi/1.760859770e11
 
-class testLLG_test(FEMTestCase):
+class LLG_test(FEMTestCase):
+    def test_stationary(self):
+        p = FEMProject(3)
+
+        # geometry
+        p.geometries.add(geometry.Box(0, 0, 0, 1, 0.1, 0.1))
+        p.mesher.setRefinement(0)
+
+        # model: boundary and initial conditions
+        model = llg.LLGModel()
+        model.initialConditions.append(InitialCondition("Initial condition1", [1/np.sqrt(2), 0, 1/np.sqrt(2)], [1]))
+        p.models.append(model)
+
+        # solver
+        solver = StationarySolver([model], [GMRESSolver()])
+        p.solvers.append(solver)
+
+        # solve
+        mf.run(p)
+
+        # solution
+        sol = FEMSolution(".", p)
+        res = sol.eval("mx", data_number=1)
+        for w in res:
+            self.assert_array_almost_equal(w.data, np.zeros(w.data.shape), decimal=2)
+
     def test_precession(self):
         factor = 1
         p = FEMProject(3)
@@ -123,24 +148,4 @@ class testLLG_test(FEMTestCase):
                 x = x - np.linalg.inv(J).dot(Axb)
             x0 = x
         return x0
-
-    def test_precession3(self):
-        return
-        p = FEMProject(3)
-
-        # geometry
-        p.geometries.add(geometry.Box(0, 0, 0, 1, 0.1, 0.1))
-        p.mesher.setRefinement(2)
-
-        # model: boundary and initial conditions
-        model = llg.LLGModel()
-        model.initialConditions.append(InitialCondition("Initial condition1", [1, 0, 0], [1]))
-        p.models.append(model)
-
-        # solver
-        solver = TimeDependentSolver([model], [BackwardEulerSolver(GMRESSolver())], T/1000, T/20)
-        p.solvers.append(solver)
-
-        # solve
-        mf.run(p)
 
