a44d8d2bb2078b17f00f7669c7d101e7db4f56da
Nakamura_master <asuka.nakamura@riken.jp>
2023-12-10 16:36:06 +0900

    remove unnecessary files

--- lys_fem/models/Elasticity/__init__.py
+++
@@ -1,9 +0,0 @@
-from lys_fem import addMaterialParameter, addModel
-from lys_fem.mf import addMFEMModel
-from .material import ElasticParameters
-from .model import ElasticModel
-from .mfem import MFEMElasticModel
-
-addMaterialParameter("Acoustics", ElasticParameters)
-addModel("Acoustics", ElasticModel)
-addMFEMModel("Elasticity", MFEMElasticModel)
