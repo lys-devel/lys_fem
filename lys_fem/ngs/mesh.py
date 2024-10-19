4beba13a9fa7b02b205a68157720f559bb11dded
Asuka <lys.dev@gmail.com>
2024-10-20 03:50:50 +0900

    Fix a serious bug in ReadGMesh

--- lys_fem/ngs/mesh.py
+++ lys_fem/ngs/mesh.py
@@ -24,10 +24,9 @@ def generateMesh(fem, file="mesh.msh"):
     if mpi.isRoot:
         geom = fem.geometries.generateGeometry()
         fem.mesher.export(geom, file)
-        gmesh, points = ReadGmsh(file)
+        gmesh, points = ReadGmsh(file, fem.dimension)
         if gmesh.dim == 1:
             _createBoundaryFor1D(gmesh, file, points)
-        _setBCNames(gmesh, file)
         coords = np.array(gmesh.Coordinates())
 
     if mpi.isParallel():
@@ -48,15 +47,6 @@ def generateMesh(fem, file="mesh.msh"):
     util.dx.setScale(mesh.scale)
     util.ds.setMesh(mesh)
     return mesh
-
-def _setBCNames(gmesh, file):
-    # Load file by gmsh
-    model = gmsh.model()
-    model.add("Default")
-    model.setCurrent("Default")
-    gmsh.merge(file)
-    for dim, tag in model.getEntities(gmesh.dim-1):
-        gmesh.SetBCName(tag-1, "boundary"+str(tag))
 
 def _createBoundaryFor1D(gmesh, file, points):
     # Load file by gmsh
@@ -126,21 +116,6 @@ def exportMesh(mesh, file=None):
         np.savez(file, mesh=result, coords=mesh._coords_global)
 
 
-def exportSolution(mesh, solution, file):
-    if not mpi.isParallel():
-        return np.savez(file, **solution)
-    result = {}
-    for key, sol_loc in solution.items():
-        sol_loc = mpi.gatherArray(sol_loc)
-        if mpi.isRoot:
-            sol_glob = np.empty((len(mesh._coords_global)))
-            for map, sol in zip(mesh._map_to_global, sol_loc):
-                sol_glob[map] = sol
-            result[key] = sol_glob
-    if mpi.isRoot:
-        np.savez(file, **result)
-
-
 def _gatherMesh(mesh, result):
     elem_list = {"line": 2, "quad": 4, "triangle": 3, "tetra": 4, "pyramid": 5, "prism": 6, "hexa": 8}
     result_glob = []
@@ -156,74 +131,22 @@ def _gatherMesh(mesh, result):
     return result_glob
 
 
-def ReadGmsh(filename): #from netgen.read_gmsh import ReadGmsh
+def ReadGmsh(filename, meshdim): #from netgen.read_gmsh import ReadGmsh
     if not filename.endswith(".msh"):
         filename += ".msh"
-    meshdim = 1
-    with open(filename, 'r') as f:
-        while f.readline().split()[0] != "$Elements":
-            pass
-        nelem = int(f.readline())
-        for i in range(nelem):
-            line = f.readline()
-            eltype = int(line.split()[1])
-            if eltype > 1 and eltype != 15:
-                meshdim = 2
-            if eltype > 3 and eltype != 15:
-                meshdim = 3
-                break
 
     f = open(filename, 'r')
     mesh = Mesh(dim=meshdim)
 
-    pointmap = {}
-    facedescriptormap = {}
-    namemap = { 0 : "default" }
-    materialmap = {}
-    bbcmap = {}
+    pointmap, facedescriptormap, materialmap, bbcmap = {}, {}, {}, {}
 
-    segm = 1
-    trig = 2
-    quad = 3
-    tet = 4
-    hex = 5
-    prism = 6
-    pyramid = 7
-    segm3 = 8      # 2nd order line
-    trig6 = 9      # 2nd order trig
-    tet10 = 11     # 2nd order tet
-    point = 15
-    quad8 = 16     # 2nd order quad
-    hex20 = 17     # 2nd order hex
-    prism15 = 18   # 2nd order prism
-    pyramid13 = 19 # 2nd order pyramid
-    segms = [segm, segm3]
-    trigs = [trig, trig6]
-    quads = [quad, quad8]
-    tets = [tet, tet10]
-    hexes = [hex, hex20]
-    prisms = [prism, prism15]
-    pyramids = [pyramid, pyramid13]
-    elem0d = [point]
-    elem1d = segms
-    elem2d = trigs + quads
-    elem3d = tets + hexes + prisms + pyramids
+    point, segm, trig, quad, tet, hex, prism, pyramid = 15, 1, 2, 3, 4, 5, 6, 7
+    segm3, trig6, tet10, quad8, hex20, prism15, pyramid13 = 8, 9, 11, 16, 17, 18, 19      # 2nd order line, trig, tet, quad, hex, prism, pyramid
 
-    num_nodes_map = { segm : 2,
-                      trig : 3,
-                      quad : 4,
-                      tet : 4,
-                      hex : 8,
-                      prism : 6,
-                      pyramid : 5,
-                      segm3 : 3,
-                      trig6 : 6,
-                      tet10 : 10,
-                      point : 1,
-                      quad8 : 8,
-                      hex20 : 20,
-                      prism15 : 18,
-                      pyramid13 : 19 }
+    num_nodes_map = { segm : 2, trig : 3, quad : 4, tet : 4, hex : 8, prism : 6, pyramid : 5, segm3 : 3, trig6 : 6, tet10 : 10, point : 1, quad8 : 8, hex20 : 20, prism15 : 18, pyramid13 : 19 }
+    ordering = {point: [0], segm: [0,1], segm3: [0,1,2], trig: [0,1,2], trig6: [0,1,2,4,5,3], quad: [0,1,2,3], quad8: [0,1,2,3,4,6,7,5],
+                tet: [0,1,2,3], tet10: [0,1,2,3,4,6,7,5,9,8], hex: [0,1,5,4,3,2,6,7], hex20: [0,1,5,4,3,2,6,7,8,16,10,12,13,19,15,14,9,11,18,17],
+                prism: [0,2,1,3,5,4], prism15: [0,2,1,3,5,4,7,6,9,8,11,10,13,12,14], pyramid: [3,2,1,0,4], pyramid13: [3,2,1,0,4,10,5,6,8,12,11,9,7]}
 
     while True:
         line = f.readline()
@@ -231,141 +154,66 @@ def ReadGmsh(filename): #from netgen.read_gmsh import ReadGmsh
             break
 
         if line.split()[0] == "$PhysicalNames":
-            #print('WARNING: Physical groups detected - Be sure to define them for every geometrical entity.')
-            numnames = int(f.readline())
-            for i in range(numnames):
-                f.readline
+            for _ in range(int(f.readline())):
                 line = f.readline()
-                namemap[int(line.split()[1])] = line.split()[2][1:-1]
 
         if line.split()[0] == "$Nodes":
-            num = int(f.readline().split()[0])
-            for i in range(num):
+            for _ in range(int(f.readline().split()[0])):
                 line = f.readline()
                 nodenum, x, y, z = line.split()[0:4]
                 pnum = mesh.Add(MeshPoint(Pnt(float(x), float(y), float(z))))
                 pointmap[int(nodenum)] = pnum
 
         if line.split()[0] == "$Elements":
-            num = int(f.readline().split()[0])
-
-            for i in range(num):
+            for _ in range(int(f.readline().split()[0])):
                 line = f.readline().split()
-                elmnum = int(line[0])
-                elmtype = int(line[1])
-                numtags = int(line[2])
+                elmtype, numtags, group, id = int(line[1]), int(line[2]), int(line[3]), int(line[4])
                 # the first tag is the physical group nr, the second tag is the group nr of the dim
-                tags = [int(line[3 + k]) for k in range(numtags)]
 
                 if elmtype not in num_nodes_map:
                     raise Exception("element type", elmtype, "not implemented")
-                num_nodes = num_nodes_map[elmtype]
 
-                nodenums = line[3 + numtags:3 + numtags + num_nodes]
-                nodenums2 = [pointmap[int(nn)] for nn in nodenums]
+                nodenums = line[3 + numtags:3 + numtags + num_nodes_map[elmtype]]
+                nodenums = [pointmap[int(nn)] for nn in nodenums]
+                nodenums = [nodenums[i] for i in ordering[elmtype]]
 
-                if elmtype in elem1d:
-                    if meshdim == 3:
-                        if tags[1] in bbcmap:
-                            index = bbcmap[tags[1]]
-                        else:
-                            index = len(bbcmap) + 1
-                            if len(namemap):
-                                mesh.SetCD2Name(index, namemap[tags[0]])
-                            else:
-                                mesh.SetCD2Name(index, "line" + str(tags[1]))
-                            bbcmap[tags[1]] = index
+                elem_dim = 0 if elmtype==point else (1 if elmtype in [segm, segm3] else (2 if elmtype in [trig, trig6, quad, quad8] else 3))
 
-                    elif meshdim == 2:
-                        if tags[1] in facedescriptormap.keys():
-                            index = facedescriptormap[tags[1]]
-                        else:
-                            index = len(facedescriptormap) + 1
-                            fd = FaceDescriptor(bc=index)
-                            if len(namemap):
-                                fd.bcname = namemap[tags[0]]
-                            else:
-                                fd.bcname = 'line' + str(tags[1])
-                            mesh.SetBCName(index - 1, fd.bcname)
-                            mesh.Add(fd)
-                            facedescriptormap[tags[1]] = index
-                    else:
-                        if tags[1] in materialmap:
-                            index = materialmap[tags[1]]
-                        else:
-                            index = len(materialmap) + 1
-                            if len(namemap):
-                                mesh.SetMaterial(index, namemap[tags[0]])
-                            else:
-                                mesh.SetMaterial(index, "line" + str(tags[1]))
-                            materialmap[tags[1]] = index
+                if elem_dim == 0:
+                    continue
 
-                    mesh.Add(Element1D(index=index, vertices=nodenums2))
-
-                if elmtype in elem2d:  # 2d elements
-                    if meshdim == 3:
-                        if tags[1] in facedescriptormap.keys():
-                            index = facedescriptormap[tags[1]]
-                        else:
-                            index = len(facedescriptormap) + 1
-                            fd = FaceDescriptor(bc=index)
-                            if len(namemap):
-                                fd.bcname = namemap[tags[0]]
-                            else:
-                                fd.bcname = "surf" + str(tags[1])
-                            mesh.SetBCName(index - 1, fd.bcname)
-                            mesh.Add(fd)
-                            facedescriptormap[tags[1]] = index
-                    else:
-                        if tags[1] in materialmap:
-                            index = materialmap[tags[1]]
-                        else:
-                            index = len(materialmap) + 1
-                            if len(namemap):
-                                mesh.SetMaterial(index, namemap[tags[0]])
-                            else:
-                                mesh.SetMaterial(index, "surf" + str(tags[1]))
-                            materialmap[tags[1]] = index
-
-                    if elmtype in trigs:
-                        ordering = [i for i in range(3)]
-                        if elmtype == trig6:
-                            ordering += [4,5,3]
-                    if elmtype in quads:
-                        ordering = [i for i in range(4)]
-                        if elmtype == quad8:
-                            ordering += [4, 6, 7, 5]
-                    mesh.Add(Element2D(index, [nodenums2[i] for i in ordering]))
-
-                if elmtype in elem3d:  # volume elements
-                    if tags[1] in materialmap:
-                        index = materialmap[tags[1]]
+                if elem_dim == meshdim:
+                    if id in materialmap:
+                        index = materialmap[id]
                     else:
                         index = len(materialmap) + 1
-                        if len(namemap):
-                            mesh.SetMaterial(index, namemap[tags[0]])
-                        else:
-                            mesh.SetMaterial(index, "vol" + str(tags[1]))
-                        materialmap[tags[1]] = index
+                        mesh.SetMaterial(index, "domain" + str(group))
+                        materialmap[id] = index
 
-                    nodenums2 = [pointmap[int(nn)] for nn in nodenums]
+                if elem_dim == meshdim -1:
+                    if id in facedescriptormap.keys():
+                        index = facedescriptormap[id]
+                    else:
+                        index = len(facedescriptormap) + 1
+                        fd = FaceDescriptor(bc=index)
+                        fd.bcname = 'boundary' + str(group)
+                        mesh.SetBCName(index - 1, 'boundary' + str(group))
+                        mesh.Add(fd)
+                        facedescriptormap[id] = index
 
-                    if elmtype in tets:
-                        ordering = [0,1,2,3]
-                        if elmtype == tet10:
-                            ordering += [4,6,7,5,9,8]
-                    elif elmtype in hexes:
-                        ordering = [0,1,5,4,3,2,6,7]
-                        if elmtype == hex20:
-                            ordering += [8,16,10,12,13,19,15,14,9,11,18,17]
-                    elif elmtype in prisms:
-                        ordering = [0,2,1,3,5,4]
-                        if elmtype == prism15:
-                            ordering += [7,6,9,8,11,10,13,12,14]
-                    elif elmtype in pyramids:
-                        ordering = [3,2,1,0,4]
-                        if elmtype == pyramid13:
-                            ordering += [10,5,6,8,12,11,9,7]
-                    mesh.Add(Element3D(index, [nodenums2[i] for i in ordering]))
+                if elem_dim == meshdim -2:
+                    if id in bbcmap:
+                        index = bbcmap[id]
+                    else:
+                        index = len(bbcmap) + 1
+                        mesh.SetCD2Name(index, "edge" + str(group))
+                        bbcmap[id] = index
+
+                if elem_dim == 1:
+                    mesh.Add(Element1D(index=index, vertices=nodenums))
+                elif elem_dim==2:
+                    mesh.Add(Element2D(index, nodenums))
+                elif elem_dim==3:
+                    mesh.Add(Element3D(index, nodenums))
 
     return mesh, pointmap
