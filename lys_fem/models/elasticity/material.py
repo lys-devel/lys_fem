ee8dcd635db23b49dc0ac020c1355531f7a1e2cd
Nakamura-RIKEN <asuka.nakamura@riken.jp>
2025-12-18 15:10:55 +0900

    Add piezo-stress coef in elasticity

--- lys_fem/models/elasticity/material.py
+++ lys_fem/models/elasticity/material.py
@@ -7,17 +7,20 @@ from lys_fem import FEMParameter, Coef
 class ElasticParameters(FEMParameter):
     name = "Elasticity"
 
-    def __init__(self, rho=1, C=[1, 1], type="lame", alpha=None, d_e=None, d_h=None):
+    def __init__(self, rho=1, C=[1, 1], type="lame", alpha=None, d_e=None, d_h=None, e_piezo=None, piezo_type="isotropic"):
         self["rho"] = Coef(rho, description="Density (kg/m^3)")
         self["C"] = _ElasticConstantCoef(C, type)
         self["alpha"] = Coef(None if alpha is None else np.array(alpha), shape=(3,3), description="Thermal expansion coef. (1/K)", default=np.eye(3).tolist())
         self["d_e"] = Coef(d_e, description="DP coef. for electron (eV)", default=10)
         self["d_h"] = Coef(d_h, description="DP coef. for hole (eV)", default=10)
+        self["e_piezo"] = _PiezoelectricStressCoef(e_piezo, piezo_type)
 
     def saveAsDictionary(self):
         d = super().saveAsDictionary()
         d["C"] = self["C"].C
         d["type"] = self["C"].type
+        d["e_piezo"] = self["e_piezo"].e
+        d["piezo_type"] = self["e_piezo"].type
         return d
 
 
@@ -88,3 +91,62 @@ class _ElasticConstantCoef(Coef):
     def widget(self):
         from .widgets import ElasticConstWidget
         return ElasticConstWidget(self)
+    
+
+class _PiezoelectricStressCoef(Coef):
+    def __init__(self, expr, type="isotropic", shape=(3,3,3), description="Piezoelectric stress coef. (C/m2)", default=1):
+        super().__init__(None, shape=shape, description=description, default=default)
+        self.e = np.array(expr).tolist() if expr is not None else None
+        self.type = type
+
+    @property
+    def expression(self):
+        if self.e is None:
+            return None
+        return self.__get_e(self._construct_e())
+
+    def setValid(self, b=True):
+        if b:
+            self.e = self.default
+        else:
+            self.e = None
+
+    def _construct_e(self):
+        if self.type == "isotropic":
+            return [[self.e if i==j else 0 for j in range(6)] for i in range(3)]
+        elif self.type == "cubic":
+            res = np.zeros((3,6))
+            res[0,4] = res[1,3] = res[2,5] = self.e
+            return res
+        elif self.type == "orthorhombic":
+            e = float(self.e[0]), float(self.e[1]), float(self.e[2])
+            return [[e[i] if i==j else 0 for j in range(6)] for i in range(3)]
+        elif self.type in ["monoclinic", "triclinic"]:
+            return self.e
+        else:
+            e31, e33, e15 = float(self.e[0]), float(self.e[1]), float(self.e[2])
+            res = np.zeros((3,6))
+            res[2,0] = res[2,1] = e31
+            res[2,2] = e33
+            res[1,3] = res[0,4] = e15
+            return res.tolist()
+
+    def __get_e(self, e):
+        res, e = np.zeros((3,3,3)), np.array(e)
+        for i,j,k in itertools.product(range(3),range(3),range(3)):
+            res[k,i,j] = e[k][self.__map(i,j)]
+        return res.tolist()
+    
+    def __map(self, i, j):
+        if i == j:
+            return i
+        if (i == 0 and j == 1) or (i == 1 and j == 0):
+            return 3
+        if (i == 1 and j == 2) or (i == 2 and j == 1):
+            return 4
+        if (i == 0 and j == 2) or (i == 2 and j == 0):
+            return 5
+
+    def widget(self):
+        from .widgets import PiezoConstWidget
+        return PiezoConstWidget(self)
--- lys_fem/models/elasticity/widgets.py
+++ lys_fem/models/elasticity/widgets.py
@@ -94,3 +94,108 @@ class _ElasticConstMatrix(MatrixFunctionWidget):
                 self._value[i][j].setText("0")
                 self._value[j][i].setText("0")
 
+
+class PiezoConstWidget(QtWidgets.QWidget):
+    def __init__(self, param):
+        super().__init__()
+        self._param = param
+        self.__initlayout()
+
+    def __initlayout(self):
+        d = ["isotropic", "cubic", "tetragonal", "orthorhombic", "hexagonal", "trigonal","monoclinic", "triclinic"]
+        self._type = QtWidgets.QComboBox()
+        self._type.addItems(d)
+        self._type.setCurrentText(self._param.type)
+        self._type.currentTextChanged.connect(self.__changeMode)
+
+        if self._param.type in ["isotropic", "cubic"]:
+            value = [self._param.e] * 3
+        elif self._param.type == ["monoclinic", "triclinic"]:
+            value = self._param.e[0,0], self._param.e[1,1], self._param.e[2,2]
+        else:
+            value = self._param.e
+        self._e1 = ScalarFunctionWidget(value[0], valueChanged=self.__set, label="Piezoelectric stress const. e11 (C/m2)")
+        self._e2 = ScalarFunctionWidget(value[1], valueChanged=self.__set, label="Piezoelectric stress const. e22 (C/m2)")
+        self._e3 = ScalarFunctionWidget(value[2], valueChanged=self.__set, label="Piezoelectric stress const. e33 (C/m2)")
+
+        self._e14 = ScalarFunctionWidget(value[0], valueChanged=self.__set, label="Piezoelectric stress const. e14 (C/m2)")
+
+        self._e31 = ScalarFunctionWidget(value[0], valueChanged=self.__set, label="Piezoelectric stress const. e31 (C/m2)")
+        self._e33 = ScalarFunctionWidget(value[1], valueChanged=self.__set, label="Piezoelectric stress const. e33 (C/m2)")
+        self._e15 = ScalarFunctionWidget(value[2], valueChanged=self.__set, label="Piezoelectric stress const. e15 (C/m2)")
+
+        self._mon = _PiezoConstMatrix("Piezoelectric stress const. (C/m2)", "monoclinic", self._param.e, valueChanged=self.__set)
+        self._p = _PiezoConstMatrix("Piezoelectric stress const. (C/m2)", "triclinic", self._param.e, valueChanged=self.__set)
+
+        self._widgets = [self._e1, self._e2, self._e3, self._e14, self._e31, self._e33, self._e15, self._mon, self._p]
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self._type)
+        layout.addWidget(self._e1)
+        layout.addWidget(self._e2)
+        layout.addWidget(self._e3)
+        layout.addWidget(self._e14)
+        layout.addWidget(self._e31)
+        layout.addWidget(self._e33)
+        layout.addWidget(self._e15)
+        layout.addWidget(self._mon)
+        layout.addWidget(self._p)
+        self.setLayout(layout)
+        self.__changeMode()
+
+    def __changeMode(self):
+        self.__set()
+        for w in self._widgets:
+            w.hide()
+        if self._type.currentText() == "isotropic":
+            self._e1.show()
+        elif self._type.currentText() == "cubic":
+            self._e14.show()
+        elif self._type.currentText() == "orthorhombic":
+            self._e1.show()
+            self._e2.show()
+            self._e3.show()
+        elif self._type.currentText() == "monoclinic":
+            self._mon.show()
+        elif self._type.currentText() == "triclinic":
+            self._p.show()
+        else:
+            self._e31.show()
+            self._e33.show()
+            self._e15.show()
+
+    def __set(self):
+        self._param.type = self._type.currentText()
+        if self._type.currentText() == "isotropic":
+            self._param.e = self._e1.value()
+        elif self._type.currentText() == "cubic":
+            self._param.e = self._e14.value()
+        elif self._type.currentText() == "orthorhombic":
+            self._param.e = [self._e1.value(), self._e2.value(), self._e3.value()]
+        elif self._type.currentText() == "monoclinic":
+            self._param.e = self._mon.value()
+        elif self._type.currentText() == "triclinic":
+            self._param.e = self._p.value()
+        else:
+            self._param.e = [self._e31.value(), self._e33.value(), self._e15.value()]
+
+
+class _PiezoConstMatrix(MatrixFunctionWidget):
+    def __init__(self, label, type, value, valueChanged=None, **kwargs):
+        self._type = type
+        super().__init__(value if np.array(value).shape==(3,6) else np.ones((3,6)), label=label, valueChanged=None, **kwargs)
+        self._combo.setCurrentText("Full")
+        self._combo.hide()
+        self._combo.currentTextChanged.connect(self._changeType)
+        if valueChanged is not None:
+            super().valueChanged.connect(valueChanged)
+
+    def _changeType(self):
+        super()._changeType()
+        if self._type == "monoclinic":
+            for i in range(3):
+                self._value[i][3].setEnabled(False)
+                self._value[i][5].setEnabled(False)
+                self._value[i][3].setText("0")
+                self._value[i][5].setText("0")
