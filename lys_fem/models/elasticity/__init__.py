d2c8696a9126bd982ff62f90c9aa691a87fa9995
Nakamura-RIKEN <asuka.nakamura@riken.jp>
2026-01-16 16:52:49 +0900

    Fix a bug

--- lys_fem/models/elasticity/__init__.py
+++ lys_fem/models/elasticity/__init__.py
@@ -1,6 +1,6 @@
 from lys_fem import addMaterialParameter, addModel
 
-from ..common import DirichletBoundary, InitialCondition, UserDefinedParameters
+from ..common import DirichletBoundary, NeumannBoundary, InitialCondition, UserDefinedParameters
 from .material import ElasticParameters
 from .model import ElasticModel, InitialCondition, ThermoelasticStress, DeformationPotential, PerfectlyMatchedLayer, InversePiezoelectricity
 
